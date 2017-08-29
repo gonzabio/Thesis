3f43d2ae1162f52f95f7c6b2957bae83873456b4
Alexandra Gonzalez <angonzalez@usfca.edu>
2017-08-28 23:27:13 -0700

    Create GonzalezMap Practice custom icon, practice coordinates and map - (hasnt worked yet)

--- Leaflet Practice .R
+++ Leaflet Practice .R
@@ -34,3 +34,69 @@ library(maps)
 mapStates = map("state", fill = TRUE, plot = FALSE)
 leaflet(data = mapStates) %>% addTiles() %>%
   addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
+
+oerioerjgoreijg
+
+
+
+library(raster)
+library(leaflet)
+
+
+#Leaflet is one of the most populat javascript libraries for creating interactive maps. 
+mymap <- leaflet () %>%
+  addTiles()
+mymap #blank map 
+
+#Add markers 
+
+mymap <- mymap %>%
+  addMarkers(lat=39.2980803, lng = -76.5898801,
+             popup = "Jeff Leek's Office")
+mymap #fixed it
+
+
+#Addnig Many Markers 
+
+# Adding one marker at a time is often not practical if you
+# want to display many markers. If you have a data frame with
+# columns 'lat' and 'lng' you can pip ethat data frame into 
+# 'leaflet()' to add all the points at once. 
+
+set.seed(2016-04-25)
+df <- data.frame (lat = runif (20, min = 39.2, max = 39.3),
+                  lng = runif (20, min = -76.6, max = -76.5))
+
+df %>% 
+  leaflet () %>% 
+  addTiles() %>% 
+  addMarkers()
+
+
+#Making Custom Markers 
+# Adding Multiple Popups 
+
+#Me trying to make a map based on this example youtube 
+# video 
+
+GonzalezIcon <- makeIcon(
+  iconUrl = "https://s-media-cache-ak0.pinimg.com/originals/49/b1/0f/49b10f78b1c3f532cf5313e25419e71a.jpg",
+  iconWidth = 31 * 215/230, iconHeight = 31,
+  iconAnchorX = 31*215/230/2, iconAnchorY = 16
+  )
+
+Alexandra_df <- data.frame(
+  lat = c(-118.1334, -117.9190),
+  lng = c(33.9024, 33.8121)
+)
+
+Alexandra_df %>%
+  leaflet() %>%
+  addTiles() %>%
+  addMarkers(icon = GonzalezIcon)
+
+
+
+
+
+
