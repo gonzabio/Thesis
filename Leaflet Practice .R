78c6efc90bc807eb169a668193ec8fd0ecb5ed23
Alexandra Gonzalez <angonzalez@usfca.edu>
2017-08-28 21:10:38 -0700

    Create practice Leaflet map from data (also practicing with commits/saving)

--- Leaflet Practice .R
+++ Leaflet Practice .R
@@ -12,18 +12,25 @@ m <- leaflet () %>%
   addMarkers(lng=174.768, lat=-36.852, popup="Im a savage dont you forget it fam")
 m #prints the map 
 
-gonzalez_map <- leaflet ()%>%
-  addTiles() %>% #Add default OpenStreetMap map tiles 
-  addMarkers(lng = 33.8121, lat= 117.9190, popup = "The birthplace of gonzalez")
-gonzalez_map #For some reason, these coordinates dont work? 
-
-##########
-########## The Map Widget 
-
-
-#Initializing options 
-#Set value for the minZoom and mazZoom settings
+#set value for the minZoom and maxZoom settings 
 leaflet(options = leafletOptions(minZoom = 0, maxZoom = 18))
-?leafletOptions
+df = data.frame(Lat = 1:10, Long = rnorm(10))
+leaflet(df) %>% addCircles()
+leaflet(df) %>% addCircles(lng = ~Long, lat = ~Lat)
 
-?setView
+
+library(sp)
+Sr1 = Polygon(cbind(c(2, 4, 4, 1, 2), c(2, 3, 5, 4, 2)))
+Sr2 = Polygon(cbind(c(5, 4, 2, 5), c(2, 3, 2, 2)))
+Sr3 = Polygon(cbind(c(4, 4, 5, 10, 4), c(5, 3, 2, 5, 5)))
+Sr4 = Polygon(cbind(c(5, 6, 6, 5, 5), c(4, 4, 3, 3, 4)), hole = TRUE)
+Srs1 = Polygons(list(Sr1), "s1")
+Srs2 = Polygons(list(Sr2), "s2")
+Srs3 = Polygons(list(Sr4, Sr3), "s3/4")
+SpP = SpatialPolygons(list(Srs1, Srs2, Srs3), 1:3)
+leaflet(height = "300px") %>% addPolygons(data = SpP)
+
+library(maps)
+mapStates = map("state", fill = TRUE, plot = FALSE)
+leaflet(data = mapStates) %>% addTiles() %>%
+  addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
