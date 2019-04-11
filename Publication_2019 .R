4fe06813658e73346cfed4840dbdfa50b5ea5402
Alexandra Gonzalez <angonzalez@usfca.edu>
2019-04-11 15:57:47 -0700

    Adds text to 2019 describing other files from 2018 and which pieces are important

--- Publication_2019 .R
+++ Publication_2019 .R
@@ -1,7 +1,13 @@
 #Publication Data ~ Preparing for a manuscript 
 #Start Date: April 11, 2019 
 
-#From the looks of the scrips as to when they were last saved, To turn in and Southeast Asia Data2 
+#From the looks of the scripts as to when they were last saved, 1)To turn in and 2)Southeast Asia Data2 
 #seem to be the most reliable. 
 
 #Actually, the data that Dr. Francis-Lyon helped me with is primarily on Southeast Asia Data2
+
+
+#Scripts DataAnalysis 2018 and Data Analysis_Final I believe I redid all of the
+#analyses. That is where one can follow the trail as to how i had all of this data. 
+#Data Anlysis 2018 shows how the data was aquired and processed. Do not look at the analysis that are further
+#down in that script. 
